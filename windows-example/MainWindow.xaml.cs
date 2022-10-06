8beebe8fd8d755d8099dd03aff6ae5937449316a
luckyyuqiang <luckyyuqiang@163.com>
2022-10-06 13:31:51 +0800

    adjust demo code for windows sdk.

--- windows-example/MainWindow.xaml.cs
+++ windows-example/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace windows_example
 {
@@ -12,9 +11,6 @@ namespace windows_example
     /// </summary>
     public partial class MainWindow : Window
     {
-        // Initialize the Appkey used by the SDK, the value here is for test, if the formal environment needs to use your application Appkey
-        private static readonly string APPKEY = "41117440#383391";
-
         private readonly System.Windows.Threading.Dispatcher Dip = null;
         private static readonly HttpClient client = new HttpClient();
 
@@ -55,25 +51,13 @@ namespace windows_example
         // sign in btn click
         private async void SignIn_Click(object sender, RoutedEventArgs e)
         {
-            if (UserIdTextBox.Text.Length == 0 || PasswordTextBox.Text.Length == 0)
+            if (UserIdTextBox.Text.Length == 0 || AgoraTokenBox.Text.Length == 0)
             {
                 AddLogToLogText("username or password is null");
                 return;
             }
 
             // TODO：
-        }
-
-        // sign up btn click
-        private async void SignUp_Click(object sender, RoutedEventArgs e)
-        {
-            if (UserIdTextBox.Text.Length == 0 || PasswordTextBox.Text.Length == 0)
-            {
-                AddLogToLogText("username or password is null");
-                return;
-            }
-            
-            // TODO:
         }
 
         // sign out btn click
@@ -109,59 +93,6 @@ namespace windows_example
                 LogTextBox.Text += DateTime.Now + ": " + log + "\n";
                 LogTextBox.ScrollToEnd();
             });
-        }
-
-        // fetch agora token from app server by username and password.
-        private async Task<string> LoginToAppServer(string username, string password)
-        {
-            Dictionary<string, string> values = new Dictionary<string, string>();
-            values.Add("userAccount", username);
-            values.Add("userPassword", password);
-            string jsonStr = JsonConvert.SerializeObject(values);
-            HttpContent content = new StringContent(jsonStr);
-            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
-            HttpResponseMessage response = await client.PostAsync("https://a41.chat.agora.io/app/chat/user/login", content);
-            try
-            {
-                response.EnsureSuccessStatusCode();
-                var responseString = await response.Content.ReadAsStringAsync();
-                Dictionary<string, string> dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseString);
-                return dict["accessToken"];
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
-
-        // regist account to user server by username and password.
-        private async Task<Boolean> RegisterToAppServer(string username, string password)
-        {
-    
-            Dictionary<string, string> values = new Dictionary<string, string>();
-            values.Add("userAccount", username);
-            values.Add("userPassword", password);
-            string jsonStr = JsonConvert.SerializeObject(values);
-            HttpContent content = new StringContent(jsonStr);
-            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
-            HttpResponseMessage response = await client.PostAsync("https://a41.chat.agora.io/app/chat/user/register", content);
-            try
-            {
-                response.EnsureSuccessStatusCode();
-                var responseString = await response.Content.ReadAsStringAsync();
-                Dictionary<string, string> dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseString);
-                if (dict["code"] == "RES_OK")
-                {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            }
-            catch (Exception)
-            {
-                return false;
-            }
         }
     }
 }
